e8e55e2f6e630793f79bf6071a7c67c53a4ccdce
Guest A <Guest A@DESKTOP-DH221ES>
2018-12-10 22:29:39 -0700

    Initial commit.

---
+++ src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+
+/**
+ *
+ * @author Guest A
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("HelloAlan Ponce");
+        System.out.println("Alan Ponce");
+        //Lo que acabamos de hacer es un programa donde lo que escribas en tu println se pase output del ejercicio y te repita lo que acbas de escribir
+    }
+    
+}
